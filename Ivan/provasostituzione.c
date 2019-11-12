cc13cd08b1e57c837b44cde522142b85d8d0e5dd
IvanDSElite <57435548+IvanDSElite@users.noreply.github.com>
2019-11-12 16:48:14 +0100

    Add files via upload

---
+++ Ivan/provasostituzione.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+char frase[256];
+char parola[100];     //parola chiave
+char alfabeto[26]={"abcdefghijklmnopqrstuvwxyz"};
+
+void sostituzione () {
+  printf("Inserire una parola di sole minuscole e senza ripetizioni di lettere:\n");
+  scanf("%s", parola);
+
+  for (int i=0; i<strlen(parola); i++) {
+  alfabeto[i]=parola[i];}
+     for (int j=0;i<=strlen(alfabeto); i++)
+
+
+
+  printf("%s\n", alfabeto);
+}
+
+int main () {
+  printf("Inserire una frase:\n");
+  fgets(frase, sizeof(frase), stdin);
+
+sostituzione ();
+
+printf("%s", frase);
+  return 0;
+}
