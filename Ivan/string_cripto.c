f4023aec9eb29c5f59928a108dc2af451ba96c3c
IvanDSElite <57435548+IvanDSElite@users.noreply.github.com>
2019-11-12 17:25:34 +0100

    Add files via upload

---
+++ Ivan/string_cripto.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+char frase[256];
+
+void cesare () {
+  int k;             //chiave di cifratura da inserire
+  int i;
+  int n=26;           //riporta a valori normali i char oltre il valore 122
+
+  printf("Inserire una chiave di cifratura: ");
+  scanf("%d", &k);
+
+  if (k >= 1 && k<= 26) {               //condizione della chiave
+  for (i=0; i<strlen(frase); i++)
+    if (frase[i] + k > 122)          //superamento del valore di 122
+      printf("%c", (frase[i]+ k) - n);
+      else if (frase[i] + k >= 32 && frase[i] + k <= 96)  //valori da non modificare
+      printf("%c", (frase[i] + k) - k);
+      else printf("%c", frase[i] + k);   //valori accettabili
+}
+    else printf("Inserire una chiave tra i valori 1 e 26\n");
+
+    printf("\n");
+}
+
+
+int main () {
+
+  printf("Inserire la frase da criptare in sole lettere minuscole:\n"); //plaintext
+  fgets(frase, sizeof(frase), stdin);
+
+  cesare ();
+
+  return 0;
+
+}
