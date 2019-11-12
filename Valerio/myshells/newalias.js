66a15732918f979c26dd8b11a1a48de3700637e7
Valerio Molinari <mr.anderson2159@MBP-di-Valerio.station>
2019-11-12 20:52:01 +0100

    aggiornamento "newalias.js"

--- Valerio/myshells/newalias.js
+++ Valerio/myshells/newalias.js
@@ -1,9 +1,20 @@
+//Funziona solo sui sistemi POSIX
+
+if (process.platform === 'win32')
+  return console.log('Unfortunately this program doesn\'t work on window, change OS ;)');
+
 const fs = require('fs')
 const { execSync } = require("child_process")
-const bash_profile = process.platform === 'darwin' ? '~/.zshenv' : '~/.bash_aliases'
+const dirsplit = __dirname.split('/')
+const mainFolder = dirsplit.slice(0, 3).join('/')
+if (dirsplit[dirsplit.length - 1] !== 'myShells')
+  return console.log(`\nError: newalias.js must be located in ${mainFolder}/myshells to work, now it is in ${__dirname}\n`);
+const bash_profile = process.platform === 'darwin' ? `${mainFolder}/.zshenv` : `${mainFolder}/.bash_aliases`
 const error = 'Error: 2 parameters expected (alias and body) but'
 if (process.argv[2] === '--edit')
   return execSync(`atom ${__filename}`)
+if (process.argv[2] === '-i')
+  return console.log(`\nusage: newalias [alias name] "[alias body]"\n\nreturns -> alias [alias name]='[alias body]' inside ${bash_profile}\n`);
 if (!process.argv[2])
   throw `${error} none recieved`
 if (!process.argv[3])
