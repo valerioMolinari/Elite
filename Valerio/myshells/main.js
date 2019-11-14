cd732bc8f8377019579d625de6460988a34f0407
Valerio Molinari <mr.anderson2159@MBP-di-Valerio.station>
2019-11-14 20:57:49 +0100

    aggiornamento main.js

--- Valerio/myshells/main.js
+++ Valerio/myshells/main.js
@@ -8,12 +8,49 @@ const fs = require('fs')
 const { exec } = require("child_process")
 
 const [flag, fileName] = process.argv.slice(2)
+const libraries = {
+  stdio: {
+    flag: 'n',
+    include: "#include <stdio.h>"
+  }
+}
+
+const newLib = (name, flag, include) => libraries[name] = { flag, include }
+
+newLib('stdlib', 'l', '#include <stdlib.h>')
+newLib('time', 't', '#include <time.h>')
+newLib('unistd', 'u', '#include <unistd.h>')
+newLib('math', 'm', '#include <math.h>')
+newLib('string', 's', '#include <string.h>')
+
+const flagArr = Object.entries(libraries).map(x => x[1].flag)
+const getLib = (flag) => Object.entries(libraries).filter(x => x[1].flag == flag)[0]
 
 let message = 'usage: [-i, -n, --edit]'
 message += '\n\t-i: show this message'
 message += '\n\t-n: create new file.c with stdio.h and main inside'
 message += '\n\t-s: create new file.c with stdio.h, stdlib.h, main and system("clear") inside'
 message += '\n\t--edit: edit this program in atom'
+
+if (!(/^-/.test(flag)))
+  throw `Error: first argument must be a flag but recieved ${flag}\n\n${message}`
+
+if (flag === '--edit')
+  exec(`atom ${__filename}`)
+else if (flag === '-i')
+  console.log(message);
+else {
+  const slicedFlag = flag.slice(1)
+  for (letter of slicedFlag)
+    if (flagArr.indexOf(letter) === -1)
+      throw `Error: -${letter} is an unknown flag\n\n${message}`
+  let head = libraries.stdio.include;
+  for (letter of slicedFlag)
+    head += '\n' + getLib(letter)[1].include
+  return console.log(head);
+
+}
+
 
 switch (flag) {
   case '--edit':
@@ -26,7 +63,7 @@ switch (flag) {
     main(/\.c$/.test(fileName) ? fileName.slice(0, fileName.length - 2) : fileName, '#include <stdio.h>\n\nint main(void) {\n\n}')
     break;
   case '-s':
-    main(/\.c$/.test(fileName) ? fileName.slice(0, fileName.length - 2) : fileName, '#include <stdio.h>\n#include <stdlib.h>\n\nint main(void) {\n\tsystem("clear")\n\n}')
+    main(/\.c$/.test(fileName) ? fileName.slice(0, fileName.length - 2) : fileName, '#include <stdio.h>\n#include <stdlib.h>\n\nint main(void) {\n\tsystem("clear");\n\n}')
     break;
   default:
     console.error('Error: unknown flag')
