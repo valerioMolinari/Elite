49e15eb171c09a637eadb3e89fe349e4409f641c
Valerio Molinari <mr.anderson2159@MBP-di-Valerio.station>
2019-11-14 23:27:06 +0100

    Chiedi la frase da criptare come prima cosa

--- Bernardino/C/esonero/esonero.c
+++ Bernardino/C/esonero/esonero.c
@@ -3,42 +3,39 @@
 #include <string.h>
 
 int main(void) {
-system("clear");
+  system("clear");
 
-int k; //chiave k del cifrario di sostituzione
-char stringa[256]; // frase inserita da tastiera ancora da criptare
-char frasecriptata[256]; //frase criptata mediante il cifrario di Cesare
-int scelta; //valore di scelta fra cifrario di Cesare e cifrario a sostituzione
+  int k; //chiave k del cifrario di sostituzione
+  char stringa[256]; // frase inserita da tastiera ancora da criptare
+  char frasecriptata[256]; //frase criptata mediante il cifrario di Cesare
+  int scelta; //valore di scelta fra cifrario di Cesare e cifrario a sostituzione
 
-     printf("Per utilizzare il cifrario di Cesare premere il tasto 1\n");
-     printf("Per utilizzare il cifrario a sostituzione premere il tasto 2\n");
-     scanf("%d", &scelta);
+  printf("Inserisci la frase che vuoi cifrare:\n");
+  fgets (stringa, sizeof(stringa), stdin);
 
-     if(scelta == 1){
+  printf("Per utilizzare il cifrario di Cesare premere il tasto 1\n");
+  printf("Per utilizzare il cifrario a sostituzione premere il tasto 2\n");
+  scanf("%d", &scelta);
 
-        system("clear");
-        printf("Inserisci il valore della chiave k:\n");
-        scanf("%d", &k);
+  if (scelta == 1) {
+    system("clear");
 
-        printf("Inserisci la frase che vuoi cifrare:\n");
-        while('\n'!=getchar());
-        fgets (stringa, sizeof(stringa), stdin);
+    printf("Inserisci il valore della chiave k:\n");
+    scanf("%d", &k);
 
-        for (size_t i = 0; i < strlen(stringa) - 1; i++) {
-          if(stringa[i] >= 97 && stringa[i] <= 122){
-
-            char frasecriptata = 97 + (((stringa[i]%97)+k)%26);
-            printf("La frase inserita criptata con il cifrario di Cesare è: %s", frasecriptata);
-
-          }
-        }
+    for (size_t i = 0; i < strlen(stringa) - 1; i++) {
+      if(stringa[i] >= 97 && stringa[i] <= 122) {
+        char frasecriptata = 97 + (((stringa[i]%97)+k)%26);
+        printf("La frase inserita criptata con il cifrario di Cesare è: %s", frasecriptata);
       }
+    }
+  }
 
-     if(scelta == 2){
+  if(scelta == 2) {
 
-     }
+  }
 
-     if(scelta != 1 && scelta != 2){
-       printf("Premere un tasto valido all'esecuzione del programma\n");
-     }
+  if(scelta != 1 && scelta != 2){
+     printf("Premere un tasto valido all'esecuzione del programma\n");
+   }
 }
