491aa442ac7a45c7842a2ed9a96d792ead95dffa
bernaElite1 <57435350+bernaElite1@users.noreply.github.com>
2019-11-14 23:04:12 +0100

    Update esonero.c

--- Bernardino/C/esonero/esonero.c
+++ Bernardino/C/esonero/esonero.c
@@ -1,17 +1,44 @@
-/* Chiedere all'utente di scegliere se utilizzare il cifrario di Cesare oppure
-utilizzare il cifrario con l'alfabeto rimescolato;
-se l'utente sceglie il cifrario di Cesare allora:
-fissare una variabile k;
-chiedere all'utente di inserire di quante posizioni k traslare l'alfabeto classico;
-in base al valore di k traslare l'alfabeto;
-chiedere all'utente di inserire una stringa di massimo 256 caratteri;
-leggere la stringa inserita da tastiera;
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
 system("clear");
 
+int k; //chiave k del cifrario di sostituzione
+char stringa[256]; // frase inserita da tastiera ancora da criptare
+char frasecriptata[256]; //frase criptata mediante il cifrario di Cesare
+int scelta; //valore di scelta fra cifrario di Cesare e cifrario a sostituzione
+
+     printf("Per utilizzare il cifrario di Cesare premere il tasto 1\n");
+     printf("Per utilizzare il cifrario a sostituzione premere il tasto 2\n");
+     scanf("%d", &scelta);
+
+     if(scelta == 1){
+
+        system("clear");
+        printf("Inserisci il valore della chiave k:\n");
+        scanf("%d", &k);
+
+        printf("Inserisci la frase che vuoi cifrare:\n");
+        while('\n'!=getchar());
+        fgets (stringa, sizeof(stringa), stdin);
+
+        for (size_t i = 0; i < strlen(stringa) - 1; i++) {
+          if(stringa[i] >= 97 && stringa[i] <= 122){
+
+            char frasecriptata = 97 + (((stringa[i]%97)+k)%26);
+            printf("La frase inserita criptata con il cifrario di Cesare è: %s", frasecriptata);
+
+          }
+        }
+      }
+
+     if(scelta == 2){
+
+     }
+
+     if(scelta != 1 && scelta != 2){
+       printf("Premere un tasto valido all'esecuzione del programma\n");
+     }
 }
