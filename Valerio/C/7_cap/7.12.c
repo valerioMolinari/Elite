1ec77414e8c89973b6680b649313fd2b6c0fee11
Valerio Molinari <mr.anderson2159@MBP-di-Valerio.station>
2019-11-17 15:13:55 +0100

    tris

--- Valerio/C/7_cap/7.12.c
+++ Valerio/C/7_cap/7.12.c
@@ -8,6 +8,10 @@
 
 void mischia(unsigned int *wDeck);
 
+void swap(unsigned int *ptr1, unsigned int *ptr2);
+
+void module13Sort(unsigned int * const array, const size_t size);
+
 void stampaCarte(unsigned int *wDeck, const char *wFace[], const char *wSuit[], size_t size);
 
 void creaMano(unsigned int *wDeck, unsigned int *hand);
@@ -29,9 +33,6 @@ unsigned int poker(unsigned int *wHand);
 unsigned int scalaReale(unsigned int *wHand);
 
 
-
-
-
 int main(void) {
 	system("clear");
 	srand(time(NULL));
@@ -47,13 +48,13 @@ int main(void) {
 	mischia(deck);
 	creaMano(deck, hand);
 
-	stampaCarte(deck, face, suit, CARDS);
+	//stampaCarte(deck, face, suit, CARDS);
 	stampaCarte(hand, face, suit, HAND);
 
 	printf("\n%s\n", coppia(hand) ? "Coppia" : "Nessun punto");
+	printf("\n%s\n", tris(hand) ? "Tris" : "Nessun punto");
 
 	puts("");
-	return 0;
 }
 
 void mischia(unsigned int *wDeck) {
@@ -68,11 +69,26 @@ void mischia(unsigned int *wDeck) {
 	}
 }
 
+void swap(unsigned int *ptr1, unsigned int *ptr2) {
+	unsigned int hold = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = hold;
+}
+
+void module13Sort(unsigned int * const array, const size_t size) {
+	for (size_t i = 0; i < size - 1; i++) {
+		for (size_t j = 0; j < size - 1; j++)
+			if (array[j] % 13 > array[j + 1] % 13) {
+				swap(&array[j], &array[j + 1]);
+			}
+	}
+}
+
 void stampaCarte(unsigned int *wDeck, const char *wFace[], const char *wSuit[], size_t size) {
 	for (size_t card = 0; card < size; card++) {
 		const char *face = wFace[(wDeck[card] - 1) % 13];
 		const char *suit = wSuit[(wDeck[card] - 1) / 13];
-		printf("%5s di %-8s%c", face, suit, card % 2 ? '\n' : '\t');
+		printf("%5s di %-8s\n", face, suit);
 	}
 	puts("");
 }
@@ -80,12 +96,24 @@ void stampaCarte(unsigned int *wDeck, const char *wFace[], const char *wSuit[], 
 void creaMano(unsigned int *wDeck, unsigned int *wHand) {
 	for (size_t i = 0; i < 5; i++)
 		wHand[i] = wDeck[i];
+	module13Sort(wHand, 5);
 }
 
 unsigned int coppia(unsigned int *wHand) {
 	for (size_t i = 0; i < 4; i++)
-		for (size_t j = i + 1; j < 5; j++)
-			if ((wHand[i] % 13) == (wHand[j] % 13))
-				return 1;
+		if ((wHand[i] % 13) == (wHand[i + 1] % 13))
+			return 1;
 	return 0;
+}
+
+unsigned int tris(unsigned int *wHand) {
+	unsigned int count = 0;
+	for (size_t i = 0; i < 4; i++) {
+		if ((wHand[i] % 13) == (wHand[i + 1] % 13)) {
+			count++;
+			if (count == 2) break;
+		} else
+			count = 0;
+	}
+	return count == 2 ? 3 : 0;
 }
