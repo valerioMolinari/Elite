11a1bdf033f7888bce8ec5e24e863668127f35b4
Delle Monache Giorgio <giorgenzo@MBP-di-Delle.fritz.box>
2019-11-17 11:28:23 +0100

    Esonero finito (manca qualche funzione per la frase iniziale)

--- Gens/C/string_cripto.c
+++ Gens/C/string_cripto.c
@@ -1,19 +1,20 @@
-#include<stdio.h>  //libreria input output
-#include<stdlib.h> //libreria che dichiara funzioni e costanti di utilità
-#include<string.h> //libreria che gestisce le stringhe
-#include<ctype.h> //libreria che dichiara funzioni per la classificazione dei caratteri
+#include<stdio.h>  // libreria input output
+#include<stdlib.h> // libreria che dichiara funzioni e costanti di utilità
+#include<string.h> // libreria che gestisce le stringhe
+#include<ctype.h> // libreria che dichiara funzioni per la classificazione dei caratteri
 
 int main(void){
 
   system("clear");
-
+  // variabili usate per il codice
   int k; // chiave per decifrare a scelta dall'utente
   int cifrario; // scelta tra cifrario di Cesare e a Sostituzione
   int count = 0; // serve per il ciclo se la k è minore di 0 o maggiore di 26
 
   char frase[256]; // frase presa da tastiera da codificare
   char cripto[256]; // frase criptata
-  char alfabeto_mescolato[26]; // serve per l'alfabeto mescolato a scelta dell'utente
+  char alfabeto_mescolato[27] = {0}; // serve per l'alfabeto mescolato a scelta dell'utente
+  const char alfabeto_inglese[] = "abcdefghijklmnopqrstuvwxyz"; //alfabeto inglese
 
   printf("Immettere la frase da crittografare:\n");
   fgets(frase, sizeof(frase), stdin); // input di una stringa
@@ -31,7 +32,7 @@ int main(void){
 
   system("clear");
 
-  // inizio cifrario di cesare: l'utente dovrà scegliere la chiave di cifratura e la frase verrà crittografata in base alla sua scelta
+  // inizio cifrario di cesare: l'utente dovrà scegliere la chiave di cifratura e la frase verrà cifrata in base alla sua scelta
   if(cifrario == 1){
     printf("Hai scelto il Cifrario di Cesare\n");
 
@@ -55,20 +56,29 @@ int main(void){
 
     }while(count == 0);
 
-  // inizio cifrario a sostituzione: l'utente dovrà immettere l'alfabeto a scelta sua poi verrà cifrata la frase in base alla sua scelta
+  // inizio cifrario a sostituzione: l'utente dovrà immettere l'alfabeto mescolato poi verrà criptata la frase in base alla sua scelta
   }else if(cifrario == 2){
      printf("Hai scelto Il Cifrario a sostituzione\n");
      printf("Immettere l'alfabeto mescolato a scelta dell'utente:\n");
+     scanf("%s", alfabeto_mescolato);
 
-     for (size_t i = 0; i < 26; i++) {
-       scanf("%c", &alfabeto_mescolato[i]);
-     }
-      for (size_t i = 0; i < 26; i++) {
-        for (size_t j = i + 1; j < 26; j++) {
-          if(alfabeto_mescolato[i] == alfabeto_mescolato[j]) {
-            printf("lol\n");
+     int p = 0;
+
+     // ciclo per il controllo di ripetizione degli elementi nell'array dell'alfabeto
+     for(int i = 0; i < 26; i++){
+       for (int l = i + 1; l < 25; l++){
+         if(alfabeto_mescolato[l] != alfabeto_inglese[i] && p == 0){
+           printf("L'alfabeto può essere utilizzato\n");
+           p = 1;
         }
       }
     }
+        for(int i = 0; i < frase[i]; i++ ){
+          if(frase[i] >= 97 && frase[i] <= 122 ){
+            cripto[i] = alfabeto_mescolato[frase[i] - 97];
+          }
+        }
+        system("clear");
+        printf("La frase criptata è: %s", cripto);
+    }
   }
-}
