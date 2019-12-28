c0131f6914271092e97fd7364962bda1ae718ff3
Valerio Molinari <mr.anderson2159@valerio.station>
2019-12-28 19:24:50 +0100

    commit esame ivan

--- Ivan/gamelib.c
+++ Ivan/gamelib.c
@@ -1,86 +1,84 @@
 #include "gamelib.h"
 
-static struct caverna *Pca = NULL;        //primo cunicolo arvais
-static struct caverna *Pch = NULL;    //primo cunicolo hartornen;
+static struct Caverna *Pca = NULL;        //primo cunicolo arvais
+static struct Caverna *Pch = NULL;    //primo cunicolo hartornen;
 static struct Scavatrice *arvais;
 static struct Scavatrice *hartornen;
-void crea_cunicoli ();
-static void ins_caverna (struct caverna **Pc);
-static void stampa_lista (struct caverna **Pc);
-static void canc_caverna (struct caverna **Pc);
+static void ins_caverna (struct Caverna **Pc);
+static void stampa_lista (struct Caverna **Pc);
+static void canc_caverna (struct Caverna **Pc);
 static int prob ();    //probabilità
-static void stato_caverna (struct caverna **Pc);   //tipo caverna
-static void fmelassa (struct caverna **Pc);  // randomizzazione quantità melassa
-static void fimprevisto (struct caverna **Pc);  // randomizzazione imprevisto
+static void stato_caverna (struct Caverna **Pc);   //tipo caverna
+static void fmelassa (struct Caverna **Pc);  // randomizzazione quantità melassa
+static void fimprevisto (struct Caverna **Pc);  // randomizzazione imprevisto
 static int probabilita;
 static void turnoArvais ();
 static void turnoHartornen ();
-static void avanza (struct caverna **Pc, struct Scavatrice **fam);
-static void abbatti (struct caverna **Pc, struct Scavatrice **fam);
+static void avanza (struct Caverna **Pc, struct Scavatrice **fam);
+static void abbatti (struct Caverna **Pc, struct Scavatrice **fam);
 
-static void ins_caverna (struct caverna **Pc) {
+static void ins_caverna (struct Caverna **Pc) {
   int scelta;
-//Dichiarazione nuova caverna
-struct caverna *nc =(struct caverna*) malloc(sizeof(struct caverna));  //nuova caverna
+  //Dichiarazione nuova caverna
+  struct Caverna *nc =(struct Caverna*) malloc(sizeof(struct Caverna));  //nuova caverna
   printf("Inserire un valore di a\n");
-  scanf("%d", &(nc-> a));
-  nc-> avanti = NULL;
-  nc-> destra = NULL;
-  nc-> sinistra = NULL;
+  scanf("%d", &(nc->a));
+  nc->avanti = NULL;
+  nc->destra = NULL;
+  nc->sinistra = NULL;
 
-//Menù direzione creazione nuova caverna
-printf("Inserire una direzione\n");
-printf("1 per creare una caverna di fronte\n");
-printf("2 per creare una caverna a destra\n");
-printf("3 per creare una caverna a sinistra\n");
-scanf("%d", &scelta);
+  //Menù direzione creazione nuova caverna
+  printf("Inserire una direzione\n");
+  printf("1 per creare una caverna di fronte\n");
+  printf("2 per creare una caverna a destra\n");
+  printf("3 per creare una caverna a sinistra\n");
+  scanf("%d", &scelta);
 
-    if (*Pc == NULL) {
-      *Pc = nc;
-      stato_caverna (&Pca);
-      fmelassa (&Pca);
-      fimprevisto (&Pca);
-      }
-      else {
-  switch (scelta) {
-    //Caverna avanti
-    case 1:
-      nc-> avanti = *Pc;
-      nc-> destra = NULL;
-      nc-> sinistra = NULL;
-      *Pc = nc;
-      stato_caverna (&Pca);
-      fmelassa (&Pca);
-      fimprevisto (&Pca);
-    break;
-    //caverna a destra
-    case 2:
-      nc-> avanti = NULL;
-      nc-> destra = *Pc;
-      nc-> sinistra = NULL;
-      *Pc = nc;
-      stato_caverna (&Pca);
-      fmelassa (&Pca);
-      fimprevisto (&Pca);
-    break;
-    //caverna a sinistra
-    case 3:
-      nc-> avanti = NULL;
-      nc-> destra = NULL;
-      nc-> sinistra = *Pc;
-      *Pc = nc;
-      stato_caverna (&Pca);
-      fmelassa (&Pca);
-      fimprevisto (&Pca);
-      break;
+  if (*Pc == NULL) {
+    *Pc = nc;
+    stato_caverna (&Pca);
+    fmelassa (&Pca);
+    fimprevisto (&Pca);
+  } else {
+    switch (scelta) {
+      //Caverna avanti
+      case 1:
+        nc->avanti = *Pc;
+        nc->destra = NULL;
+        nc->sinistra = NULL;
+        *Pc = nc;
+        stato_caverna (&Pca);
+        fmelassa (&Pca);
+        fimprevisto (&Pca);
+        break;
+        //caverna a destra
+      case 2:
+        nc->avanti = NULL;
+        nc->destra = *Pc;
+        nc->sinistra = NULL;
+        *Pc = nc;
+        stato_caverna (&Pca);
+        fmelassa (&Pca);
+        fimprevisto (&Pca);
+        break;
+        //caverna a sinistra
+      case 3:
+        nc->avanti = NULL;
+        nc->destra = NULL;
+        nc->sinistra = *Pc;
+        *Pc = nc;
+        stato_caverna (&Pca);
+        fmelassa (&Pca);
+        fimprevisto (&Pca);
+        break;
       //case 4:
         //return;
       default:
         printf("Comando non valido");
-      break;
-           }
-     }
-     system("clear");
+        break;
+    }
+  }
+  system("clear");
 }
 
 void crea_cunicoli () {
@@ -91,7 +89,7 @@ void crea_cunicoli () {
     //menù scelta iniziale
   printf("Effettuare una scelta\n");
   printf("Premere 1 per creare cunicolo\n");
-  printf("Premere 2 per cancellare un cunicolo precedente\n");
+  printf("Premere 2 per cancellare una caverna precedente\n");
   printf("Premere 3 per stampare i cunicoli creati\n");
   printf("Premere 4 per uscire\n");
   scanf("%d", &scelta);
@@ -144,47 +142,47 @@ void crea_cunicoli () {
   }
 }
 
-static void stampa_lista (struct caverna **Pc) {
+static void stampa_lista (struct Caverna **Pc) {
   if(*Pc == NULL) {
       printf("Non ci sono caverne\n"); }
   else {
-    struct caverna* sonda = *Pc;
+    struct Caverna* sonda = *Pc;
 
     do {
       if (sonda->tipo_caverna == 0)
-        printf("Valore: %d, Tipo: Normale\n", sonda-> a);
+        printf("Valore: %d, Tipo: Normale\n", sonda->a);
       else if (sonda->tipo_caverna == 1)
-        printf("Valore: %d, Tipo: Speciale\n", sonda-> a);
+        printf("Valore: %d, Tipo: Speciale\n", sonda->a);
       else
-        printf("Valore: %d, Tipo: Accidentata\n", sonda-> a);
-          if (sonda-> avanti != NULL)
-            sonda = sonda -> avanti;
-          else if (sonda-> destra != NULL)
-            sonda = sonda -> destra;
+        printf("Valore: %d, Tipo: Accidentata\n", sonda->a);
+          if (sonda->avanti != NULL)
+            sonda = sonda ->avanti;
+          else if (sonda->destra != NULL)
+            sonda = sonda ->destra;
           else
-            sonda = sonda -> sinistra;
-        //sonda = sonda -> destra;
-        //sonda = sonda -> sinistra;
+            sonda = sonda ->sinistra;
+        //sonda = sonda ->destra;
+        //sonda = sonda ->sinistra;
     }while(sonda != NULL);
   }
       return;
 }
 
-static void canc_caverna (struct caverna **Pc) {
+static void canc_caverna (struct Caverna **Pc) {
   if (*Pc == NULL)
     printf("Non ci sono caverne\n");
-  else if ((*Pc)-> avanti != NULL) {
-      struct caverna *temp = (*Pc)-> avanti;
+  else if ((*Pc)->avanti != NULL) {
+      struct Caverna *temp = (*Pc)->avanti;
       free(*Pc);
       *Pc = temp;
     }
-  else if ((*Pc)-> destra != NULL) {
-      struct caverna *temp = (*Pc)-> destra;
+  else if ((*Pc)->destra != NULL) {
+      struct Caverna *temp = (*Pc)->destra;
       free(*Pc);
       *Pc = temp;
   }
   else {
-      struct caverna *temp = (*Pc)-> sinistra;
+      struct Caverna *temp = (*Pc)->sinistra;
       free(*Pc);
       *Pc = temp;
   }
@@ -192,7 +190,7 @@ static void canc_caverna (struct caverna **Pc) {
     return;
   }
 
-static void stato_caverna (struct caverna **Pc) {
+static void stato_caverna (struct Caverna **Pc) {
   int scelta;
   //Menù stato caverna
   printf("Di che tipo deve essere la caverna?\n");
@@ -203,13 +201,13 @@ static void stato_caverna (struct caverna **Pc) {
 
   switch (scelta) {
     case 1:
-      (*Pc)-> tipo_caverna = normale;
+      (*Pc)->tipo_caverna = normale;
       break;
     case 2:
-      (*Pc)-> tipo_caverna = speciale;
+      (*Pc)->tipo_caverna = speciale;
       break;
     case 3:
-      (*Pc)-> tipo_caverna = accidentata;
+      (*Pc)->tipo_caverna = accidentata;
       break;
     case 4:
       printf("Comando non valido\n");
@@ -224,7 +222,7 @@ static int prob () {
   return probabilita;
 }
 
-static void fmelassa (struct caverna **Pc) {
+static void fmelassa (struct Caverna **Pc) {
   probabilita = 0;
   prob ();
   if (probabilita <= 50)
@@ -235,15 +233,15 @@ static void fmelassa (struct caverna **Pc) {
     (*Pc)->melassa = molta;
 }
 
-static void fimprevisto (struct caverna **Pc) {
+static void fimprevisto (struct Caverna **Pc) {
   probabilita = 0;
   prob ();
   if (probabilita <= 50)
-    (*Pc)-> imprevisto = nessun_imprevisto;
+    (*Pc)->imprevisto = nessun_imprevisto;
   else if (probabilita > 50 && probabilita <= 85)
-    (*Pc)-> imprevisto = crollo;
+    (*Pc)->imprevisto = crollo;
   else if (probabilita > 85 && probabilita <= 100)
-    (*Pc)-> imprevisto = baco;
+    (*Pc)->imprevisto = baco;
 }
 
 static void turnoArvais () {
@@ -251,9 +249,9 @@ static void turnoArvais () {
   int ppu = 0;   //probabilità progressiva uscita
   //stato giocatore Arvais
   printf("Turno Arvais\n");
-  printf("Quantità serbatoio energia %d/4\n", arvais-> serbatoio_energia);
-  printf("Quantità serbatoio raccolta %d/10\n", arvais-> serbatoio_raccolta);
-  
+  printf("Quantità serbatoio energia %d/4\n", arvais->serbatoio_energia);
+  printf("Quantità serbatoio raccolta %d/10\n", arvais->serbatoio_raccolta);
+
   //Menù decisionale arvais
   printf("Cosa vuoi fare?\n");
   printf("Premere 1 per avanzare\n");
@@ -288,8 +286,8 @@ static void turnoArvais () {
 static void turnoHartornen () {
   int scelta;
   printf("Turno Hartornen\n");
-  printf("Quantità serbatoio energia %d/4\n", hartornen-> serbatoio_energia);
-  printf("Quantità serbatoio raccolta %d/10\n", hartornen-> serbatoio_raccolta);
+  printf("Quantità serbatoio energia %d/4\n", hartornen->serbatoio_energia);
+  printf("Quantità serbatoio raccolta %d/10\n", hartornen->serbatoio_raccolta);
 
 
   printf("Cosa vuoi fare?\n");
@@ -317,82 +315,65 @@ static void turnoHartornen () {
   }
 }
 
-static void avanza (struct caverna **Pc, struct Scavatrice **fam) {
-  *fam = *Pc;
+static void avanza (struct Caverna **Pc, struct Scavatrice **fam) {
+  (*fam)->posizione = *Pc;
 //puntatore non nullo = direzione
-  if ((*Pc)-> avanti != NULL) {
+  if ((*Pc)->avanti != NULL) {
     printf("Sei andato avanti\n");
-    (*fam) = (*Pc)-> avanti;
+    (*fam)->posizione = (*Pc)->avanti;
   }
-  else if ((*Pc)-> destra != NULL) {
+  else if ((*Pc)->destra != NULL) {
     printf("Hai svoltato a destra\n");
-    *fam = (*Pc)-> destra;
+    (*fam)->posizione = (*Pc)->destra;
   }
   else {
     printf("Hai svoltato a sinistra\n");
-    *fam = (*Pc)-> sinistra;
+    (*fam)->posizione = (*Pc)->sinistra;
   }
 
-//tipo imprevisto
-  if ((*Pc)-> imprevisto == nessun_imprevisto) {
-        (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia;
-        (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
-      }
-  else if ((*Pc)-> imprevisto == crollo) {
+  //tipo imprevisto
+  if ((*Pc)->imprevisto == crollo) {
     printf("C'è stato un crollo: consumi 1 punto energia per liberarti dalle macerie\n");
-    (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia - 1;
-    (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
+    (*fam)->serbatoio_energia--;
   }
-  else {
+  if ((*Pc)->imprevisto == baco) {
     printf("Hai incontrato un feroce baco: sei salvo, ma perdi tutti i punti energia e raccolta accumulati\n");
-    (*fam)-> serbatoio_energia = 0;
-    (*fam)-> serbatoio_raccolta = 0;
+    (*fam)->serbatoio_energia = 0;
+    (*fam)->serbatoio_raccolta = 0;
   }
-// tipo caverna
-  if ((*Pc)-> tipo_caverna == normale) {
-    (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia;
-    (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
-  }
-  else if ((*Pc)-> tipo_caverna == speciale) {
+  // tipo caverna
+  if ((*Pc)->tipo_caverna == speciale) {
     printf("La caverna irradia melassa: guadagni 1 punto energia\n");
-    (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia + 1;
-    (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
+    (*fam)->serbatoio_energia++;
   }
-  else if ((*Pc)-> tipo_caverna == accidentata) {
+  if ((*Pc)->tipo_caverna == accidentata) {
     printf("La caverna è accidentata: perdi 1 punto energia\n");
-    (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia -1;
-    (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
+    (*fam)->serbatoio_energia--;
   }
   //else
     //uscita
 
     //quantità melassa
-  if ((*Pc)-> melassa == nessuna) {
-    (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia;
-    (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
-  }
-  else if ((*Pc)-> melassa == poca) {
+
+  if ((*Pc)->melassa == poca) {
     int scelta;
     printf("La caverna contiene poca melassa: guadagni 1 unita di melassa\n");
     printf("In quale serbatoio vuoi mettere la melassa?\n");
     printf("Premere 1 per metterla nel serbatoio di energia\n");
     printf("Premere 2 per metterla nel serbatoio di raccolta\n");
     scanf("%d", &scelta);
-
     switch (scelta) {
       case 1:
-        (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia + 1;
-        (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
-    break;
+        (*fam)->serbatoio_energia++;
+        break;
       case 2:
-        (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia;
-        (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta +1;
-    break;
-    default:
+        (*fam)->serbatoio_raccolta++;
+        break;
+      default:
         printf("Comando non valido\n");
-      }
+    }
   }
-  else {
+  if ((*Pc)->melassa == molta) {
     int scelta2;
     printf("La caverna contiene molta melassa: guadagni 3 unita di melassa\n");
     printf("In quale serbatoio vuoi mettere la melassa?\n");
@@ -401,27 +382,25 @@ static void avanza (struct caverna **Pc, struct Scavatrice **fam) {
     scanf("%d", &scelta2);
 
     switch (scelta2) {
-    case 1:
-        (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia + 3;
-        (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta;
-    break;
-    case 2:
-        (*fam)-> serbatoio_energia = (*fam)-> serbatoio_energia;
-        (*fam)-> serbatoio_raccolta = (*fam)-> serbatoio_raccolta + 3;
-    break;
-    default:
+      case 1:
+        (*fam)->serbatoio_energia += 3;
+        break;
+      case 2:
+        (*fam)->serbatoio_raccolta += 3;
+        break;
+      default:
         printf("Comando non valido\n");
+    }
   }
- }
 }
 
-static void abbatti (struct caverna **Pc, struct Scavatrice **fam) {
+static void abbatti (struct Caverna **Pc, struct Scavatrice **fam) {
   int scelta;
   //Dichiarazione nuova caverna
-struct caverna *abbatti = (struct caverna*)malloc(sizeof(struct caverna));
-abbatti-> avanti = NULL;
-abbatti-> destra = NULL;
-abbatti-> sinistra = NULL;
+struct Caverna *abbatti = (struct Caverna*)malloc(sizeof(struct Caverna));
+abbatti->avanti = NULL;
+abbatti->destra = NULL;
+abbatti->sinistra = NULL;
 
 printf("Dove vuoi abbattere?\n");
 printf("Premere 1 per abbattere avanti\n");
@@ -430,32 +409,32 @@ printf("Premere 3 per abbattere a sinistra\n");
 
 switch (scelta) {
   case 1:
-    if ((*Pc)-> avanti != NULL)
+    if ((*Pc)->avanti != NULL)
         printf("Spiacente, esiste già una caverna in quella direzione");
     else {
-        abbatti-> avanti = *Pc;
-        abbatti-> destra = NULL;
-        abbatti-> sinistra = NULL;
+        abbatti->avanti = *Pc;
+        abbatti->destra = NULL;
+        abbatti->sinistra = NULL;
         *Pc = abbatti;
     }
   break;
   case 2:
-    if ((*Pc)-> destra != NULL)
+    if ((*Pc)->destra != NULL)
         printf("Spiacente, esiste già una caverna in quella direzione");
     else {
-        abbatti-> avanti = NULL;
-        abbatti-> destra = *Pc;
-        abbatti-> sinistra = NULL;
+        abbatti->avanti = NULL;
+        abbatti->destra = *Pc;
+        abbatti->sinistra = NULL;
         *Pc = abbatti;
     }
   break;
   case 3:
-   if ((*Pc)-> sinistra != NULL)
+   if ((*Pc)->sinistra != NULL)
         printf("Spiacente, esiste già una caverna in quella direzione");
    else {
-        abbatti-> avanti = NULL;
-        abbatti-> destra = NULL;
-        abbatti-> sinistra = *Pc;
+        abbatti->avanti = NULL;
+        abbatti->destra = NULL;
+        abbatti->sinistra = *Pc;
         *Pc = abbatti;
    }
    break;
--- Ivan/gamelib.h
+++ Ivan/gamelib.h
@@ -2,18 +2,20 @@
 #include <time.h>
 #include <stdlib.h>
 
-struct Scavatrice {
-  struct Scavatrice *posizione;
-  int serbatoio_energia;    //valore massimo 4
-  int serbatoio_raccolta;   //valore massimo 10
-};
-
-struct caverna {
+struct Caverna {
   int a;
-  struct caverna *avanti;
-  struct caverna *destra;
-  struct caverna *sinistra;
+  struct Caverna *avanti;
+  struct Caverna *destra;
+  struct Caverna *sinistra;
   enum melassa {nessuna, poca, molta = 3} melassa;
   enum imprevisto {nessun_imprevisto, crollo, baco} imprevisto;
   enum tipo_caverna {normale, speciale, accidentata, uscita} tipo_caverna;
 };
+
+struct Scavatrice {
+  struct Caverna *posizione;
+  int serbatoio_energia;    //valore massimo 4
+  int serbatoio_raccolta;   //valore massimo 10
+};
+
+void crea_cunicoli ();
