6015105bb48926234afc45387403bbb7598eb14b
Valerio Molinari <mr.anderson2159@valerio.station>
2020-02-28 20:27:24 +0100

    modifiche

--- Valerio/myshells/classes.js
+++ Valerio/myshells/classes.js
@@ -6,7 +6,7 @@ const { execSync } = require("child_process")
 const cap = s => s.slice(0, 1).toUpperCase().concat(s.slice(1));
 const classes = process.argv.slice(2).map(c => cap(c));
 const slice = s => (/\..*$/.test(s) ? s.slice(0, -s.match(/\..*$/)[0].length) : s)
-const usage = `Utilizzo: classes [fileNames[,.ieap]]
+const usage = `Utilizzo: classes [fileNames[,.ieap]]\n
   classes main:
   classes name.m: crea un file contenente class Main o Name e
                   il metodo main all'interno;
