a9ccea683004d23ebcb62865056e2a6f08cf5505
CrisUnipg <57435273+CrisUnipg@users.noreply.github.com>
2020-04-17 00:18:50 +0200

    Add files via upload

---
+++ Cristian/C/Progetto_2019/VERSIONI_progetto_2019/V3/main.c
@@ -0,0 +1,75 @@
+//CERAMI CRISTIAN
+//MATRICOLA: 324075
+
+#include "gamelib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+  {
+    short int scelta_menu = 0, flag_termina_gioco = 0, flag_mappa_cunicoli = 0;
+    green();
+/*    for (int i = 0; i<19; i++)
+      {
+        static char nome_file [40] = "Trivella[.].txt";
+        nome_file[9] = i + 65;
+        printfile(nome_file);
+      }
+*/
+
+    do {
+         printfile("Menu_iniziale.txt");
+         blu();
+         printf("COSA VUOI FARE?\n");
+         green();
+         printf("Scelta_prima_while: %hd\n",scelta_menu);
+
+         //Controllo dell'input
+         while (scanf("%hd",&scelta_menu) != 1 || scelta_menu >3 || scelta_menu <1)
+           {
+             blu();
+             printf("SCELTA NON CORRETTA! REINSERISCI IL VALORE: ");
+             while (getchar() != '\n');
+             green();
+           }
+
+        printf("Scelta_dopo_while: %hd\n",scelta_menu);
+        switch (scelta_menu)
+           {
+             case 1: //se ho scelto crea_cunicoli()
+                flag_mappa_cunicoli = crea_cunicoli();
+                break;
+             case 2: //se ho scelto gioca()
+             printf("Sono entrato nel case 2!\n");
+                if (flag_mappa_cunicoli == 0)
+                  {
+                    blu();
+                    printf("DEVI PRIMA CREARE I CUNICOLI!\n");
+                    green();
+                    sleep(2);
+                    break;
+                  }
+                gioca();
+                break;
+             case 3: //se ho scelto termina_gioco()
+                flag_termina_gioco = termina_gioco();
+                system("clear");
+                for (int a=1; a<=4; a++)
+                  {
+                    if (a%2 == 0)
+                      red();
+                    else
+                      yellow();
+                    print_nome();
+                    sleep(2);
+                    print_matricola();
+                    sleep(2);
+                    if (a == 4)
+                      break;
+                    system("clear");
+                  }
+                break;
+           }
+       } while(flag_termina_gioco != 1);
+    return (0);
+  }
