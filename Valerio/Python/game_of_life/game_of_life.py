1dfe52dc79298e782b89076c37bb327dca691e7f
Valerio Molinari <DavidGooss@iMac-di-Valerio.station>
2020-08-04 16:42:25 +0200

    game_of_life

---
+++ Valerio/Python/game_of_life/game_of_life.py
@@ -0,0 +1,18 @@
+import pyglet
+from AppKit import NSScreen
+import numpy as np
+
+def give_birth(row, col):
+    matrix[row, col] = 1
+
+def kill(row, col):
+    matrix[row, col] = 0
+
+def get_neighboorhood(row, col):
+    pass
+
+WIDTH = int(NSScreen.mainScreen().frame().size.width)
+HEIGHT = int(NSScreen.mainScreen().frame().size.height)
+SQUARES_SIZE = 10
+
+matrix = np.zeros((WIDTH // SQUARES_SIZE, HEIGHT // SQUARES_SIZE), dtype=np.uint8)
