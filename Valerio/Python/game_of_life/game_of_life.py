9f07befa048ad7eb84b4022a3499bf62603cc4cc
valerioMolinari <valerio.molinari@studenti.unipg.it>
2020-08-04 17:55:30 +0200

    .

--- Valerio/Python/game_of_life/game_of_life.py
+++ Valerio/Python/game_of_life/game_of_life.py
@@ -14,5 +14,6 @@ def get_neighboorhood(row, col):
 WIDTH = int(NSScreen.mainScreen().frame().size.width)
 HEIGHT = int(NSScreen.mainScreen().frame().size.height)
 SQUARES_SIZE = 10
+WIDTH, HEIGHT
 
 matrix = np.zeros((WIDTH // SQUARES_SIZE, HEIGHT // SQUARES_SIZE), dtype=np.uint8)
